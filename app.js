3033362ec88000d513b9551a0dad0308b08b62e9
amina <saburjanovaamina@gmail.com>
2024-10-12 09:37:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/*git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Amina066/lesson3.git
+git push -u origin main */
+
+//ctrl+j//
